636a9abf48056d35c666d7c05ca76438199c323f
root <root@Soothingkiwi.localdomain>
2023-02-24 19:30:07 -0800

    basic csv content and python commandline functionality

---
+++ Projects/Project01/feature_analysis.py
@@ -0,0 +1,10 @@
+from sklearn.feature_selection import RFE
+
+class FeatureAnalysis:
+    
+    def __init__(self, csv):
+        self.csv = csv
+        
+    def recursive_feature_elimination(self):
+        return 0
+        
